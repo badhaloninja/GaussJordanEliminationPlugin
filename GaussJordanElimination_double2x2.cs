6d2037d866bca91505f780aca0736cda7eab5c55
badhaloninja <badhaloninja@badhalo.ninja>
2021-12-29 21:42:32 -0600

    LogixOperator Overload example

--- GaussJordanElimination_double2x2.cs
+++ GaussJordanElimination_double2x2.cs
@@ -2,55 +2,30 @@
 using FrooxEngine.LogiX;
 using BaseX;
 using Mehroz;
-using System;
 
 namespace MatrixMod
 {
     [NodeName("Reduced Echelon Form")]
-    [NodeOverload("Reduced-Echelon-Form")]
+    [NodeOverload("ReducedEchelonForm")]
     [Category(new string[] { "LogiX/Math/Matrix" })]
 
-    public sealed class GaussJordanElimination: LogixNode
+    public sealed class GaussJordanElimination : LogixOperator<double2> //T is the node output type
     {
         public readonly Input<double2x2> LinearEquationMatrix;
         public readonly Input<double2> LinearSolutionMatrix;
-        public readonly Output<double2> SolutionMatrix;
 
-        // Code goes here!
-        protected override void OnEvaluate()
+        public override double2 Content //T is the node output type
         {
-            Matrix m1 = new Matrix(LinearEquationMatrix.EvaluateRaw().To2DArray());
-            Matrix m2 = new Matrix(2, 1);
-            m2[0, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().x);
-            m2[1, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().y);
-            Matrix m3 = Matrix.Concatenate(m1, m2);
-            m3 = m3.ReducedEchelonForm();
-            SolutionMatrix.Value = new double2(m3[0, 2].ToDouble(), m3[1, 2].ToDouble());
-        }
-
-        protected override Type FindOverload(NodeTypes connectingTypes)
-        {
-            if (this.LinearEquationMatrix.IsConnected || this.LinearSolutionMatrix.IsConnected) // If any input is connected skip
+            get // Code goes here!
             {
-                return null;
+                Matrix m1 = new Matrix(LinearEquationMatrix.EvaluateRaw().To2DArray());
+                Matrix m2 = new Matrix(2, 1);
+                m2[0, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().x);
+                m2[1, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().y);
+                Matrix m3 = Matrix.Concatenate(m1, m2);
+                m3 = m3.ReducedEchelonForm();
+                return new double2(m3[0, 2].ToDouble(), m3[1, 2].ToDouble());
             }
-            Type type;
-            if (connectingTypes.inputs.TryGetValue("LinearEquationMatrix", out type)) // Get type of wire trying to connect to input LinearEquationMatrix
-            {
-                if(type == typeof(BaseX.double3x3)) // This would perferably be done in some generic way
-                {
-                    return typeof(MatrixMod.GaussJordanElimination_double3x3);  // Return 3x3
-                }
-            }
-            if (connectingTypes.inputs.TryGetValue("LinearSolutionMatrix", out type)) // Get type of wire trying to connect to input LinearSolutionMatrix
-            {
-                if (type == typeof(BaseX.double3))
-                {
-                    return typeof(MatrixMod.GaussJordanElimination_double3x3);
-                }
-            }
-            return null;
         }
     }
 }
-
--- GaussJordanElimination_double3x3.cs
+++ GaussJordanElimination_double3x3.cs
@@ -2,57 +2,32 @@
 using FrooxEngine.LogiX;
 using BaseX;
 using Mehroz;
-using System;
 
 namespace MatrixMod
 {
     [HiddenNode] // Hide overload from node browser
     [NodeName("Reduced Echelon Form")]
-    [NodeOverload("GaussJordanElimination")]
+    [NodeOverload("ReducedEchelonForm")]
     [Category(new string[] { "LogiX/Math/Matrix" })]
 
-    public sealed class GaussJordanElimination_double3x3 : LogixNode
+    public sealed class GaussJordanElimination_double3x3 : LogixOperator<double3> //T is the node output type
     {
         public readonly Input<double3x3> LinearEquationMatrix;
         public readonly Input<double3> LinearSolutionMatrix;
-        public readonly Output<double3> SolutionMatrix;
 
-        // Code goes here!
-        protected override void OnEvaluate()
+        public override double3 Content //T is the node output type
         {
-            Matrix m1 = new Matrix(LinearEquationMatrix.EvaluateRaw().To2DArray());
-            Matrix m2 = new Matrix(3, 1);
-            m2[0, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().x);
-            m2[1, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().y);
-            m2[2, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().z);
-            Matrix m3 = Matrix.Concatenate(m1, m2);
-            m3 = m3.ReducedEchelonForm();
-            SolutionMatrix.Value = new double3(m3[0, 2].ToDouble(), m3[1, 2].ToDouble(), m3[2, 2].ToDouble());
-        }
-
-        protected override Type FindOverload(NodeTypes connectingTypes)
-        {
-            if (this.LinearEquationMatrix.IsConnected || this.LinearSolutionMatrix.IsConnected) // If any input is connected skip
+            get // Code goes here!
             {
-                return null;
+                Matrix m1 = new Matrix(LinearEquationMatrix.EvaluateRaw().To2DArray());
+                Matrix m2 = new Matrix(3, 1);
+                m2[0, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().x);
+                m2[1, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().y);
+                m2[2, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().z);
+                Matrix m3 = Matrix.Concatenate(m1, m2);
+                m3 = m3.ReducedEchelonForm();
+                return new double3(m3[0, 2].ToDouble(), m3[1, 2].ToDouble(), m3[2, 2].ToDouble());
             }
-            Type type;
-            if (connectingTypes.inputs.TryGetValue("LinearEquationMatrix", out type)) // Get type of wire trying to connect to input LinearEquationMatrix
-            {
-                if (type == typeof(BaseX.double2x2)) // This would perferably be done in some generic way
-                {
-                    return typeof(MatrixMod.GaussJordanElimination);  // Return 2x2
-                }
-            }
-            if (connectingTypes.inputs.TryGetValue("LinearSolutionMatrix", out type)) // Get type of wire trying to connect to input LinearSolutionMatrix
-            {
-                if (type == typeof(BaseX.double2))
-                {
-                    return typeof(MatrixMod.GaussJordanElimination); // Return 2x2
-                }
-            }
-            return null;
         }
     }
 }
-
